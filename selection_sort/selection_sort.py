5b9a1f77d844b5e6228a610396274f78024cb8f7
Anthony Rafidison <100955619+anthonyraf@users.noreply.github.com>
2023-02-27 17:27:08 +0000

    Add selection sort

---
+++ selection_sort/selection_sort.py
@@ -0,0 +1,30 @@
+# Author: Anthony Rafidison <benjaraf006@gmail.com>
+import random 
+
+
+class SelectionSort:
+    def __init__(self, L: list[int|float]):
+        self.list = L
+    
+    def sort(self):
+        for unsorted_index in range(len(self.list) - 1):
+            min = self.list[unsorted_index]
+            min_index = unsorted_index
+
+            for i in range(unsorted_index+1, len(self.list)):
+                if (actual := self.list[i]) < min:
+                    min = actual
+                    min_index = i
+
+            self.list[min_index] = self.list[unsorted_index]
+            self.list[unsorted_index] = min
+
+
+if __name__ == '__main__':
+    l = list(range(10))
+    random.shuffle(l)
+
+    s = SelectionSort(l)
+    s.sort()
+
+    print(s.list)
